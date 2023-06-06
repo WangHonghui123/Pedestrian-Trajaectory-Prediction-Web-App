ca57536725ee8b953926e56df6c85a94671263a6
EdieeKnows <Xu_yizhi@outlook.com>
2023-06-06 17:15:27 +1000

    create header and footer components, fix start button size, change letter spacing.

---
+++ ptp/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Row, Col, Footer } from "react-bootstrap";
+
+const infoFooter = () => {
+	return (
+		<>
+			<Container id="footer" >
+				<Row className="justify-content-end fs-6">
+					<Col sm={1}>
+						<a href="mailto:xuyizhi1996@gmail.com" className="link-info">Contact</a>
+					</Col>
+					<Col sm={1}>
+						<a href="https://github.com/WangHonghui123/Pedestrian-Trajaectory-Prediction-Web-App" className="link-info">GitHub</a>
+					</Col>
+					<Col sm={1}>
+						<p>version 0.1.0</p>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	)
+}
+
+export default infoFooter;
--- ptp/src/pages/Intro.js
+++ ptp/src/pages/Intro.js
@@ -1,20 +1,19 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import { Container, Row, Col, Button } from "react-bootstrap"
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
-//import logo image
-import logo from "../assets/logo.svg"
+// import header & Footer
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Intro = () => {
 	return (
 		<>
-			<Container>
-				<Row className="px-3">
-					<Col>
-						<img src={logo} alt="PTP" />
-						<h1 className="text-secondary">pedestrain trajactory prediction</h1>
-					</Col>
+			<Container id="centerContent">
+				<Row>
+					<Header />
 				</Row>
+
 				<Row className="p-5">
 					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam soluta nemo atque ea quasi in ipsa eos fuga, quaerat ex ad facere, quas rem distinctio nesciunt quos impedit nam enim.
 						Eveniet voluptatum magni repudiandae praesentium autem! Sunt dicta distinctio dolor aspernatur, hic maxime maiores aliquid ullam, aliquam explicabo excepturi. Commodi, deleniti assumenda perspiciatis voluptatem aut quod quam consectetur quibusdam pariatur.
@@ -22,8 +21,13 @@ const Intro = () => {
 						Cumque similique architecto accusamus suscipit explicabo ut, rem tempora aperiam voluptate a eum ipsum. Dolor voluptatum neque sed aspernatur. Eum vel maiores nisi, temporibus fuga amet. Temporibus consequatur debitis tempora.
 						Corporis sequi placeat totam aliquid id. Adipisci harum fugiat eius libero numquam ex culpa odio quisquam officia, quasi asperiores placeat quam ea esse minus saepe nostrum deleniti est quas earum.</p>
 				</Row>
+				<Row className="justify-content-center">
+					<Col className="col-2">
+						<Button variant="primary" className="text-info " href="/application">Start</Button>
+					</Col>
+				</Row>
 				<Row>
-					<Button variant="primary" className="text-info "  href="/application">Start</Button>
+					<Footer />
 				</Row>
 			</Container>
 		</>
