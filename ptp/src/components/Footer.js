d2e90e7715dcdac7e111190820181ad2c6dcb23b
EdieeKnows <Xu_yizhi@outlook.com>
2023-06-07 21:37:15 +1000

    adjust footer grid system

--- ptp/src/components/Footer.js
+++ ptp/src/components/Footer.js
@@ -6,13 +6,13 @@ const infoFooter = () => {
 		<>
 			<Container id="footer" >
 				<Row className="justify-content-end fs-6">
-					<Col sm={1}>
+					<Col sm="auto">
 						<a href="mailto:xuyizhi1996@gmail.com" className="link-info">Contact</a>
 					</Col>
-					<Col sm={1}>
+					<Col sm="auto">
 						<a href="https://github.com/WangHonghui123/Pedestrian-Trajaectory-Prediction-Web-App" className="link-info">GitHub</a>
 					</Col>
-					<Col sm={2}>
+					<Col sm="auto">
 						<p>version 0.1.0 Beta</p>
 					</Col>
 				</Row>
